b40729a6f42c14b5fe12a0e9979e2cc899a66090
Reema Patel <reemapatel97@gmail.com>
2019-04-04 12:14:03 -0400

    Updated UI of user take transit page

--- src/sample/controller/UserTakeTransit.java
+++ src/sample/controller/UserTakeTransit.java
@@ -1,8 +1,10 @@
 package sample.controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import sample.model.Transit;
 
@@ -15,8 +17,14 @@ public class UserTakeTransit {
     public TableColumn<Transit, Integer> priceCol;
     public TableColumn<Transit, String> connectedCol;
 
+    public ChoiceBox transportOptions;
+    public ChoiceBox siteOptions;
 
-    public void testing(ActionEvent actionEvent) {
+    public TextField lowPrice;
+    public TextField highPrice;
+
+
+    public void filter(ActionEvent actionEvent) {
 
         typeCol.setCellValueFactory(new PropertyValueFactory<>("type"));
         routeCol.setCellValueFactory(new PropertyValueFactory<>("route"));
@@ -25,10 +33,20 @@ public class UserTakeTransit {
 
 
         //TODO: use the FILTER command to filter results
-        
+
         Transit t1 = new Transit("hi","hi", 5, "hi");
         Transit t2 = new Transit("hello","hi", 5, "hi");
+
+        //TODO: put radio buttons newxt to each option
         transitTable.getItems().add(t1);
+
+    }
+
+    public void logTransit(ActionEvent actionEvent) {
+
+    }
+
+    public void goBack(ActionEvent actionEvent) {
 
     }
 }
