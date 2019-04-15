d44ad227006ca4fc144892f721b72fd76d65c810
Reema Patel <reemapatel97@gmail.com>
2019-04-15 18:07:44 -0400

    Finished logic for visitor event detail

--- src/sample/controller/VisitorEventDetail.java
+++ src/sample/controller/VisitorEventDetail.java
@@ -1,17 +1,27 @@
 package sample.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import sample.connectivity.ConnectionClass;
 import sample.model.Context;
 import sample.model.Event;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 public class VisitorEventDetail {
@@ -59,7 +69,7 @@ public class VisitorEventDetail {
 
     }
 
-    public void logVisit(ActionEvent actionEvent) throws SQLException {
+    public void logVisit(ActionEvent actionEvent) throws SQLException, ParseException {
         //TODO: make sure there are tickets remaining
         if (Integer.parseInt(ticketsRemaining.getText()) == 0) {
             errorMessage.setText("There are no more tickets for this event!");
@@ -68,27 +78,59 @@ public class VisitorEventDetail {
         } else if ((!visitDate.getText().trim().matches("([0-9]{4})-([0-9]{2})-([0-9]{2})"))) {
             errorMessage.setText("Visit date is not in correct format! Please follow format listed.");
         } else {
-            //TODO: check if event date is within start and end date=
-
-            //TODO: check event was not already logged today
-
-            //add entry to visit event
             ConnectionClass connectionClass = new ConnectionClass();
             Connection connection = connectionClass.getConnection();
             Statement statement = null;
             statement = connection.createStatement();
-            String sql = "CALL logVisitToEvent('" + Context.getInstance().currentUser().username
-                    + "', '" + globalEvent.getName() + "', '" + globalEvent.getStartDate() + "', '" + globalEvent.getSiteName()
-                    + "', '" + visitDate.getText().trim() + "')";
-            statement.executeUpdate(sql);
+            //TODO: check if event date is within start and end date
 
-            //update value of tickets remaining
-            ticketsRemaining.setText(Integer.toString(Integer.parseInt(ticketsRemaining.getText()) - 1));
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd");
+            Date sdate = formatter.parse(startDate.getText());
+            Date edate = formatter.parse(endDate.getText());
+            Date vdate = formatter.parse(visitDate.getText());
+            boolean isItValidDate = (vdate.after(sdate) && vdate.before(edate))|| vdate.equals(sdate) || vdate.equals(edate) ;
+            if (!isItValidDate) {
+                errorMessage.setText("Visit date is not between the start and end date!");
+            } else {
 
+
+                //TODO: check event was not already logged today
+                String sql2 = "CALL checkIfEventAlreadyLoggedToday('" + Context.getInstance().currentUser().username
+                        + "', '" + globalEvent.getName() + "', '" + globalEvent.getStartDate() + "', '" + globalEvent.getSiteName()
+                        + "', '" + visitDate.getText().trim() + "')";
+                ResultSet rs = statement.executeQuery(sql2);
+                if (rs.next()) {
+                    if (rs.getInt(1) != 0) {
+                        errorMessage.setText("You can only log a visit for the same event once a day!");
+                    } else {
+                        //add entry to visit event
+                        String sql = "CALL logVisitToEvent('" + Context.getInstance().currentUser().username
+                                + "', '" + globalEvent.getName() + "', '" + globalEvent.getStartDate() + "', '" + globalEvent.getSiteName()
+                                + "', '" + visitDate.getText().trim() + "')";
+                        statement.executeUpdate(sql);
+
+                        //update value of tickets remaining
+                        ticketsRemaining.setText(Integer.toString(Integer.parseInt(ticketsRemaining.getText()) - 1));
+                        errorMessage.setText("Event successfully logged!");
+                        errorMessage.setTextFill(Color.web("#75c24e"));
+                    }
+                }
+            }
         }
     }
 
 
     public void goBack(ActionEvent actionEvent) {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../view/visitorExploreEvent.fxml"));
+        Parent root = null;
+        try {
+            root = (Parent)fxmlLoader.load();
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
